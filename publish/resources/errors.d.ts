2d4b493c001f4a1f1ad0a5158d0309d86bf83ab2
dcmorales1987 <dcmorales1987@gmail.com>
2018-06-19 02:34:04 -0400

    update publish

---
+++ publish/resources/errors.d.ts
@@ -0,0 +1,7 @@
+export interface ErrorDictionary {
+    ARGUMENT_NULL_EXCEPTION: string;
+    ARGUMENT_UNDEFINED_EXCEPTION: string;
+    ARRAY_TYPEOFF_EXCEPTION: string;
+    EMPTY_ARRAY_EXCEPTION: string;
+}
+export declare const ERRORS: ErrorDictionary;
